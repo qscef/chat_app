28d9df272eb762f8f61bfbb15bc730c43e4ed82a
qscef <vasenkov_a_a@mail.ru>
2021-04-03 21:21:39 +0300

    new logo + delete started component vue

---
+++ src/Chatik.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <img alt="Chatik" src="./assets/company_logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chatik',
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Chatik from './Chatik.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(Chatik),
 }).$mount('#app')
